e504f2a1d981851989f56e0be13571a8c2c370bb
Zachary Soliman <zachsoliman1@gmail.com>
2022-06-17 13:36:34 -0400

    all tests pass

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+document.getElementById('main').remove()
+
+const newHeader = document.createElement('h1')
+
+newHeader.innerHTML = "<h1>YOUR-NAME is the champion</h1>"
+
+newHeader.setAttribute('id', 'victory');
